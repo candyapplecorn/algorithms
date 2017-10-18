252c6f5545968a831197daac844d292be2b2d68c
candyapplecorn <candyapplecorn@gmail.com>
2017-10-18 10:23:56 -0700

    all specs passing

--- LRU/lib/p07_practical.rb
+++ LRU/lib/p07_practical.rb
@@ -1,5 +1,24 @@
 require_relative 'p05_hash_map'
+require 'byebug'
 
 def can_string_be_palindrome?(string) 
-  
+				myHash = HashMap.new
+				string.chars.each do |c|
+								if myHash.include? c
+												val = myHash.get(c)
+												myHash.set(c, val + 1)
+								else
+												myHash.set(c, 1)
+								end
+				end
+
+				oneCount = myHash.select { |k| 
+								myHash.get(k.first) % 2 != 0 
+				}
+
+				twoCount = myHash.select {|node| 
+								myHash.get(node.first) % 2 == 0 
+				}
+
+				oneCount.length <= 1
 end

7b96c138ad5e5fc82459e3cbda11e06eec824504
App Academy Student <student@appacademy.io>
2017-10-19 09:49:10 -0700

    heap sort works, k largest works

--- Heaps/lib/heap_sort.rb
+++ Heaps/lib/heap_sort.rb
@@ -1,5 +1,22 @@
 require_relative 'heap'
+require 'byebug'
 
 class Array
-  def heap_sort!; end
+  def heap_sort!
+		maxPrc = Proc.new {|a, b| b <=> a}
+
+		each_index {|i| BinaryMinHeap.heapify_up(self, length - 1 - i, length, &maxPrc) }
+
+		counter = 0
+		while counter < self.length / 2
+			#BinaryMinHeap.heapify_down(self, counter, length - 1 - counter)
+			counter += 1
+		end
+
+		(length).times do |i|
+			BinaryMinHeap.swap(self, 0, length - 1 - i)
+			BinaryMinHeap.heapify_down(self, 0, length - 1 - i, &maxPrc)
+
+		end
+	end
 end
--- Heaps/lib/k_largest_elements.rb
+++ Heaps/lib/k_largest_elements.rb
@@ -1,3 +1,13 @@
 require_relative 'heap'
 
-def k_largest_elements(array, k); end
+def k_largest_elements(array, k)
+	bh = BinaryMinHeap.new {|a, b| b <=> a}
+
+	array.each {|i| bh.push(i) }
+	
+	largest = []
+	until largest.length == k
+		largest << bh.extract
+	end
+	largest
+end
